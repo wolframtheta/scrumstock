9fca210fb43aea007a68219ec59624b9f5f82a68
Xavier Marques <xaviermarques4f@gmail.com>
2023-10-04 09:49:44 +0200

    Remove android

--- android/app/build/intermediates/assets/release/public/7304.82f9cb560123a3b0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[7304],{7304:(s,o,n)=>{n.r(o),n.d(o,{routes:()=>h});const h=[{path:"",loadComponent:()=>Promise.all([n.e(3315),n.e(8194)]).then(n.bind(n,8194)).then(t=>t.SalesPage)},{path:"checkout",loadComponent:()=>Promise.all([n.e(8592),n.e(3037)]).then(n.bind(n,3037)).then(t=>t.CheckoutPage)}]}}]);
---
+++ android/app/src/main/java/scrumstock/com/MainActivity.java
@@ -0,0 +1,5 @@
+package scrumstock.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
