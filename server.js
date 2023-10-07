dc82cf6f2c160efa89803327e7e38e4ad9abf022
Xavier Marques <xaviermarques4f@gmail.com>
2023-10-07 10:56:15 +0200

    Install cors

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: function (origin, callback) {
-      if (!origin) {
-          return callback(null, true);
-      }
-
-      if (whitelist.indexOf(origin) === -1) {
-          var msg = 'The CORS policy for this site does not ' +
-              'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+    return callback(null, true);
   }
 }
 
