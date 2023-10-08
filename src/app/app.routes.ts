ffdf51cab439d64dd94da21563bd854ed440f419
Xavier Marques <xaviermarques4f@gmail.com>
2023-10-08 21:47:02 +0200

    Finish register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,7 +22,7 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'detail',
-    loadComponent: () => import('./pages/store/detail/detail.page').then( m => m.DetailPage)
+    path: 'privacy',
+    loadComponent: () => import('./pages/privacy/privacy.page').then( m => m.PrivacyPage)
   },
 ];
